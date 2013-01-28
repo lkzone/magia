d584ee94142df3ef25d4b2d63ee7bd71bc0eb61a
lkzone <guiixandy@hotmail.com>
2013-01-28 16:32:56 -0200

    under

---
+++ client.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import socket               # Import socket module
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+
+s.connect((host,port))
+print s.recv(1024)
+s.close                     # Close the socket when done
